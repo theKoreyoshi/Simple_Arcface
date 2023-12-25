a6ce9ac82c36435cc322189e6d77204550a3bf9c
mycode <2923446746@qq.com>
2023-12-25 22:45:30 +0800

    id_loss

--- Arcface/demo.py
+++ Arcface/demo.py
@@ -19,8 +19,8 @@ def get_img(img_path, device):
 device = 'cuda'
 img1 = get_img('data/1.png', device)
 img2 = get_img('data/2.png', device)
-img3 = get_img('data/3.png', device)
-img4 = get_img('data/4.png', device)
+# img3 = get_img('data/3.png', device)
+# img4 = get_img('data/4.png', device)
 
 print(img1.shape)    # torch.Size([1, 3, 112, 112])
 
@@ -32,16 +32,16 @@ print("Successfully loaded arcface model")
 
 emb1 = model(img1)[0]
 emb2 = model(img2)[0]
-emb3 = model(img3)[0]
-emb4 = model(img4)[0]
+# emb3 = model(img3)[0]
+# emb4 = model(img4)[0]
 print(emb1.shape)        # torch.Size([512])
 
 sim_12 = emb1.dot(emb2).item()
-sim_13 = emb1.dot(emb3).item()
-sim_24 = emb2.dot(emb4).item()
-sim_34 = emb3.dot(emb4).item()
+# sim_13 = emb1.dot(emb3).item()
+# sim_24 = emb2.dot(emb4).item()
+# sim_34 = emb3.dot(emb4).item()
 
 print(sim_12)
-print(sim_13)
-print(sim_24)
-print(sim_34)
+# print(sim_13)
+# print(sim_24)
+# print(sim_34)
